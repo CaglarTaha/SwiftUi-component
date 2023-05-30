fea159ae4a8a2ce3bb0b2f009f321de4ae2a1f30
CaglarTaha <mtaha.mtal@gmail.com>
2023-05-30 11:47:10 +0300

    Initial Commit

---
+++ Lessons/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lessons
+//
+//  Created by Muhammed Taha Çağlar on 30.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lessons/LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonsApp.swift
+//  Lessons
+//
+//  Created by Muhammed Taha Çağlar on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
